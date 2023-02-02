f4734938cd55f8ccfbaa2e11aa5a26688d470758
Ali Koheil <ali.koheil@gmail.com>
2023-02-02 19:07:02 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,31 @@
+const addSitesList = [
+    "*://*.doubleclick.net/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.googletagservices.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.google-analytics.com/*",
+    "*://*.googleadservices.com/*",
+    "*://*.zedo.com/*",
+    "*://*.adbrite.com/*",
+    "*://*.adbureau.net/*",
+    "*://*.carbonads.net/*",
+    "*://*.cdn.carbonads.com/*",
+    "*://*.cdn.carbonads.net/*",
+    "*://*.cdn.doubleclick.net/*",
+    "*://*.cdn.googletagservices.com/*",
+    "*://*.cdn.googleadservices.com/*",
+    "*://*.cdn.zedo.com/*",
+];
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        return {
+            cancel: True
+        }
+    },
+    {
+        urls: addSitesList
+    },
+    ["blocking"]
+)
+
